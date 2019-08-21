f781799c1231b306290eeb1db0e5fc62b76c0af0
haddadjo92 <haddadjo92@gmail.com>
2019-08-22 02:20:36 +0300

    Updates

---
+++ precache-manifest.c95b9e1070d1d64694c8a5a46f499eaa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7781bc8a6c3172bc083365ba97cfe2c3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b5884b56554b85dc63a4",
+    "url": "/static/css/2.2d074dc8.chunk.css"
+  },
+  {
+    "revision": "c4bcb1ec3d8b21a7a48b",
+    "url": "/static/css/main.e9639a40.chunk.css"
+  },
+  {
+    "revision": "b5884b56554b85dc63a4",
+    "url": "/static/js/2.fd0fdfd7.chunk.js"
+  },
+  {
+    "revision": "c4bcb1ec3d8b21a7a48b",
+    "url": "/static/js/main.098bc995.chunk.js"
+  },
+  {
+    "revision": "1be6857d84fdb1f0345e",
+    "url": "/static/js/runtime~main.77515d75.js"
+  },
+  {
+    "revision": "26aa74fe6aa82b0e2d0aee8e59c92cb0",
+    "url": "/static/media/react-bootstrap-logo.26aa74fe.svg"
+  }
+]);
